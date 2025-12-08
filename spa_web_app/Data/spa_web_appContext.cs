5c4d623dcbece63ad98889fbf50fbe02fee5ee56
zqnd3r <brickinthewall9@gmail.com>
2025-12-08 13:53:13 -0600

    WIP: BookAppointment feature changes

--- spa_web_app/Data/spa_web_appContext.cs
+++ spa_web_app/Data/spa_web_appContext.cs
@@ -1,10 +1,12 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.AspNetCore.Identity;
+using spa_web_app.Models;
 
 namespace spa_web_app.Data
 {
     public class spa_web_appContext(DbContextOptions<spa_web_appContext> options) : IdentityDbContext<IdentityUser>(options)
     {
+        public DbSet<Appointment> Appointments { get; set; } = default!;
     }
 }
